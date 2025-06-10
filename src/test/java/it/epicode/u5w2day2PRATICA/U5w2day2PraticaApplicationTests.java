d0a676f991a908affbcefc5d4fe18bb782cfddbc
pandino126 <re.luca95@gmail.com>
2025-06-10 16:32:55 +0200

    Initial commit

---
+++ src/test/java/it/epicode/u5w2day2PRATICA/U5w2day2PraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.u5w2day2PRATICA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w2day2PraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
